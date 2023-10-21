5ea7f59777aa3dc5d58698fdee46753bf85c9709
User <User@USER-PC>
2023-10-21 17:34:41 +0300

    Добавьте файлы проекта.

---
+++ Calculator.cpp
@@ -0,0 +1,20 @@
+#include "Calculator.h"
+
+double Calculator::calculate(double x, char oper, double y)
+{
+	switch (oper)
+	{
+	case '+':
+		return x + y;
+	case '-':
+		return x - y;
+	case '*':
+		return x * y;
+	case '/':
+		return x / y;
+	default:
+		return 0.0;
+
+	}
+}
+	
